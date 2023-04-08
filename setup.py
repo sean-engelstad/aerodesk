f03870ad2f3d6ed7508dfbb81f5e4888ef532952
seanfireball1 <sean.engelstad1@gmail.com>
2023-04-07 22:25:12 -0400

    setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from subprocess import check_output
+import sys
+
+# Numpy/mpi4py must be installed prior to installing TACS
+import numpy
+import mpi4py
+
+# Import distutils
+from setuptools import setup, find_packages
+
+setup(
+    name="aerodesk",
+    version="0.1",
+    description="Learning repo for FEA and CFD",
+    long_description_content_type="text/markdown",
+    author="Sean P. Engelstad",
+    author_email="sengeltad312@gatech.edu",
+    install_requires=["numpy", "mpi4py>=3.1.1", "scipy>=1.2.1"],
+    packages=find_packages(include=["aerodesk*"]),
+)
